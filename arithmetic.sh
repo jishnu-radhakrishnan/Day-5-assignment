351e26ae3bd2817e048b2a731cb90aa02229d1e6
jishnu-radhakrishnan <jishnurk10@gmail.com>
2022-03-27 21:08:29 +0530

    Added:arithmetic operation

---
+++ arithmetic.sh
@@ -0,0 +1,39 @@
+read -p "Enter 1st number:" a
+read -p "Enter 2nd number:" b
+read -p "Enter 3rd number:" c
+
+OP1=a+b*c
+OP2=a%b+c
+OP3=c+a/b
+OP4=a*b+c
+
+echo "1st operation= " $a "+" $b "x" $c
+echo "2nd operation= " $a "%" $b "+" $c
+echo "3rd operation= " $c "+" $a "/" $b
+echo "4th operation= " $a "x" $b "+" $c
+\n
+if((OP1>OP2 && OP1>OP3 && OP1>OP4))
+ then 
+   echo "1st operation gives maximum value"
+ elif((OP2>OP3 && OP2>OP4))
+ then 
+   echo "2nd operation gives maximum value"
+ elif((OP3>OP4))
+ then
+   echo "3rd operation gives maximum value"
+ else
+   echo "4th operation gives maximum value"
+fi
+
+if((OP1<OP2 && OP1<OP3 && OP1<OP4))
+ then
+   echo "1st operation gives minimum value"
+ elif((OP2<OP3 && OP2<OP4))
+ then
+   echo "2nd operation gives minimum value"
+ elif((OP3<OP4))
+ then
+   echo "3rd operation gives minimum value"
+ else
+   echo "4th operation gives minimum value"
+fi
